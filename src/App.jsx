df4872799f0d4e0495948891704e9f354ed49ba6
jorge302 <jlordonezh@gmail.com>
2024-08-24 17:39:20 -0500

    Conexion inicio sesión con backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -15,13 +15,31 @@ function cambiarClave(evento) {
   setClave(evento.target.value)
   }
 
-  function ingresar() {
-    if (usuario == 'admin' && clave == 'admin'){
-      alert('Ingresaste')
-    }else{
+  async function ingresar() {
+    const peticion = await fetch('http://localhost:3000/login?usuario=' + usuario + '&clave=' + clave, { credentials: 'include' })
+    if (peticion.ok) {
+      setLogueado(true)
+    } else {
       alert('Usuario o clave incorrectos')
     }
+   // if (usuario == 'admin' && clave == 'admin'){
+     // alert('Ingresaste')
+     // setLogueado(true)
+    //}else{
+      //alert('Usuario o clave incorrectos')
+    //}
   }
+
+  async function validar() {
+    const peticion = await fetch('http://localhost:3000/validar', { credentials: 'include' })
+    if (peticion.ok) {
+      setLogueado(true)
+    } 
+    }
+    useEffect(()=>{
+      validar()
+    }, [])
+  
 
   return (
     <>
